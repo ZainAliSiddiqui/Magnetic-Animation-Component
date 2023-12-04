da1e36ba3ef3f3c3f7dc2e479db55dc2ad989962
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-04 20:16:01 +0500

    Added Magnetic component in main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import { Magnetic } from "@animation";
+
 export default function App() {
-	return <div>App</div>;
+	return (
+		<div className="w-full h-screen flex flex-col items-center justify-center">
+			<Magnetic>
+				<h1 className="cursor-pointer text-[20px] bg-black text-white px-6 py-3 text-center rounded-lg">
+					HOME
+				</h1>
+			</Magnetic>
+		</div>
+	);
 }
